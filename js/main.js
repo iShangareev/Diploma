6280e6e0bedf3db69981c8a8aab5c5771eaf264e
iShangareev <ilnarshan@gmail.com>
2020-08-14 14:10:40 +0400

    create: tabs

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@ var menuButton = $(".menu-button");
 menuButton.on("click", function () {
   $(".navbar-bottom").toggleClass("navbar-bottom--visible");
 });
+
+$(document).ready(function () {
+  var tabsItem = $(".trend__header-list-item");
+  var contentItem = $(".content__item");
+
+  tabsItem.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("trend__header-list-item--active");
+    contentItem.removeClass("content__item--active");
+    $(activeContent).addClass("content__item--active");
+    $(this).addClass("trend__header-list-item--active");
+  });
+});
